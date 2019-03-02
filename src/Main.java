0ce8981142911d61cc41c264fd72f4694e69ebea
sortedmap <sortedmap@gmail.com>
2019-03-02 21:14:24 +0300

    map added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class Main
 {
     public static void main(String[] args)
     {
         String s = "fdskjlfjdslkfkldsjf";
         System.out.println(s.length());
+
+        ArrayList<Integer> map = {{
+                map.add(5)
+        }};
     }
 }
